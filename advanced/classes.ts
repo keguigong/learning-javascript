1665ae48d328fe8ca6cfd3ab31ede98426b80d95
keguigong <keguigong@gmail.com>
2020-04-28 15:31:34 +0800

    add classes.ts

--- advanced/classes.ts
+++ advanced/classes.ts
@@ -1,11 +1,18 @@
 class Animal {
-    public name: string;
-    public constructor(name) {
-        this.name = name;
-    }
-}
+  public name: string;
+  public constructor(name) {
+    this.name = name;
+  };
+};
 
-let jackAnimal = new Animal("Jack");
-console.log(jackAnimal.name);
-jackAnimal.name = 'Tom';
-console.log(jackAnimal.name);
+// let jackAnimal = new Animal("Jack");
+// console.log(jackAnimal.name);
+// jackAnimal.name = 'Tom';
+// console.log(jackAnimal.name);
+
+class SmallCat extends Animal {
+  public constructor(name) {
+    super(name);
+    console.log(this.name);
+  };
+};
